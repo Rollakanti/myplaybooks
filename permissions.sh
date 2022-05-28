0e0c1715e5485d5f09037db8dcaabc794dc96cb6
Rollakanti <sravanthi.rollakanti@gmail.com>
2022-05-28 23:53:07 +0530

    updtaede perms script

--- permissions.sh
+++ permissions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo chgrp -R tomcat /opt/tomcat
-sudo chmod -R g+r conf
-sudo chmod g+x conf
+cd /opt/tomcat/apache-tomcat-8.5.79 && sudo chmod -R g+r conf && sudo chmod g+x conf
 cd /opt/tomcat/apache-tomcat-8.5.79 && sudo chown -R tomcat webapps/ work/ temp/ logs/
 sudo update-java-alternatives -l

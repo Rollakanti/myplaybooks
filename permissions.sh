b41fdffd06e2fd65e70ed06dd609b8706f50ed72
VishwasAcademy <vishwas.tech1.academy@gmail.com>
2022-10-16 18:01:27 +0530

    added packages

--- permissions.sh
+++ permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo chgrp -R tomcat /opt/tomcat
-cd /opt/tomcat/apache-tomcat-8.5.79 && sudo chmod -R g+r conf && sudo chmod g+x conf
-cd /opt/tomcat/apache-tomcat-8.5.79 && sudo chown -R tomcat webapps/ work/ temp/ logs/
-sudo update-java-alternatives -l
+cd /opt/tomcat/ && sudo chmod -R g+r conf && sudo chmod g+x conf
+cd /opt/tomcat/ && sudo chown -R tomcat webapps/ work/ temp/ logs/
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre
